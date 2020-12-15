56f7c78e472c9089a30c5676735938e73f22437a
LeonardoC97 <72605249+LeonardoC97@users.noreply.github.com>
2020-12-15 01:15:48 +0100

    Aggiunto cambio valuta

--- progettoprogrammazione/progettoprogrammazione/src/main/java/com/esame/progettoprogrammazione/controller/CurrencyLayer.java
+++ progettoprogrammazione/progettoprogrammazione/src/main/java/com/esame/progettoprogrammazione/controller/CurrencyLayer.java
@@ -1,12 +1,10 @@
 package com.esame.progettoprogrammazione.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-
+import com.esame.progettoprogrammazione.model.ExchangeRates;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -18,84 +16,45 @@ import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.http.HttpEntity;
-import org.apache.http.ParseException;
-
-
 
 @RestController
 @CrossOrigin
 public class CurrencyLayer {
-
-    // essential URL structure is built using constants 
+    // essential URL structure is built using constants
     public static final String ACCESS_KEY = "d1cc65e2509890e1d243c316a2498449";
     public static final String BASE_URL = "http://apilayer.net/api/";
     public static final String ENDPOINT = "live";
-   
+    public static JSONObject exchangeRates = new JSONObject();
+    public static ExchangeRates exchange = new ExchangeRates();
     // this object is used for executing requests to the (REST) API
     static CloseableHttpClient httpClient = HttpClients.createDefault();
-    
-    /**
-     * 
-     * Notes:<br><br>
-     * 
-     * A JSON response of the form {"key":"value"} is considered a simple Java JSONObject.<br>
-     * To get a simple value from the JSONObject, use: <JSONObject identifier>.get<Type>("key");<br>
-     * 
-     * A JSON response of the form {"key":{"key":"value"}} is considered a complex Java JSONObject.<br>
-     * To get a complex value like another JSONObject, use: <JSONObject identifier>.getJSONObject("key")
-     * 
-     * Values can also be JSONArray Objects. JSONArray objects are simple, consisting of multiple JSONObject Objects.
-     * 
-     * 
-     */
-    
+
     // sendLiveRequest() function is created to request and retrieve the data
+    public static ExchangeRates sendLiveRequest(String s, int x) throws JSONException, ClientProtocolException, IOException {
 
-
-    public static void sendLiveRequest(){
-        
-        
-        
-        
-        // The following line initializes the HttpGet Object with the URL in order to send a request
+        // The following line initializes the HttpGet Object with the URL in order to
+        // send a request
         HttpGet get = new HttpGet(BASE_URL + ENDPOINT + "?access_key=" + ACCESS_KEY);
-        
-        try {
+        CloseableHttpResponse response = httpClient.execute(get);
+        HttpEntity entity = response.getEntity();
+        // the following line converts the JSON Response to an equivalent Java Object
+        exchangeRates = new JSONObject(EntityUtils.toString(entity));
+        exchange.setSource(exchangeRates.getString("source"));
+        exchange.setSuccess(exchangeRates.getBoolean("success"));
+        exchange.setTerms(exchangeRates.getString("terms"));
+        exchange.setPrivacy(exchangeRates.getString("privacy"));
+        exchange.setTimestamp(exchangeRates.getString("timestamp"));
+        exchange.setQuotes(exchangeRates.getJSONObject("quotes").getDouble("USD" + s)* x);
+        if(x < exchange.getQuotes()){
+            exchange.setControllo("Il cambio è stato vantaggioso!");
+        }else if(x > exchange.getQuotes()){
+            exchange.setControllo("Il cambio è stato svantaggioso!");
+        }else if(x == exchange.getQuotes()){
+            exchange.setControllo("Il cambio è uguale!");
+        }
 
-                
-            CloseableHttpResponse response =  httpClient.execute(get);
-            HttpEntity entity = response.getEntity();
-           
-            // the following line converts the JSON Response to an equivalent Java Object
-            
-            JSONObject exchangeRates = new JSONObject(EntityUtils.toString(entity));
-            
-            System.out.println("Live Currency Exchange Rates");
-            
-           // Parsed JSON Objects are accessed according to the JSON response's hierarchy, output strings are built
-           Date timeStampDate = new Date(); 
-           DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a");
-           String formattedDate = dateFormat.format(timeStampDate);
-           System.out.println("1 " + exchangeRates.getString("source") + " in GBP : " + exchangeRates.getJSONObject("quotes").getDouble("USDGBP") + " (Date: " + formattedDate + ")");   
-           System.out.println("\n");
-           response.close();
-        }
-        catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-     
+        return exchange;
+
     }
-    
-     
-    }
+
+}
--- progettoprogrammazione/progettoprogrammazione/src/main/java/com/esame/progettoprogrammazione/controller/ValutaController.java
+++ progettoprogrammazione/progettoprogrammazione/src/main/java/com/esame/progettoprogrammazione/controller/ValutaController.java
@@ -1,25 +1,37 @@
 package com.esame.progettoprogrammazione.controller;
 
-import com.esame.progettoprogrammazione.controller.CurrencyLayer;
+
+import java.io.IOException;
+
+import com.esame.progettoprogrammazione.model.ExchangeRates;
 import com.esame.progettoprogrammazione.model.Valuta;
 
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class ValutaController {
-    
-    Valuta valuta = new Valuta();
 
-    @GetMapping("/a")
-    public static void prova(){
-        CurrencyLayer.sendLiveRequest();   
+    Valuta valuta = new Valuta();
+    CurrencyLayer c = new CurrencyLayer();
+
+    @GetMapping("/currency")
+    public ExchangeRates currencyCorrente(@RequestParam String valutaFinale, int valoreIniziale) throws JSONException, ClientProtocolException, IOException {
+        ExchangeRates exchange = new ExchangeRates();
+        valuta.setValutaFinale(valutaFinale);
+        valuta.setValoreIniziale(valoreIniziale);
+        exchange = CurrencyLayer.sendLiveRequest(valuta.getValutaFinale(),valuta.getValoreIniziale());
+        return exchange;
     }
+
 
     @GetMapping("/currencyLayer/{id}")
     public Valuta prendiOggetti(@PathVariable Long id){
